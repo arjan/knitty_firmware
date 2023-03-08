5d646fd91ec8ff00ca299e31c954139d3acc8da7
Arjan Scherpenisse <arjan@scherpenisse.net>
2023-03-08 20:37:36 +0100

    A2

--- software/Knitty/A2/A2.ino
+++ software/Knitty/A2/A2.ino
@@ -4,6 +4,7 @@
 #define PIN_NEEDLE_RTL 5      // Blue,  Pattern RTL
 #define PIN_NEEDLE_LTR 6      // ,  Pattern LTR
 
+
 void setup() {
     Serial.begin(115200);
 
@@ -17,8 +18,109 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(PIN_B), interruptCalibrate, CHANGE);
 }
 
+unsigned char patternLength = 20;
+signed int firstNeedle = 20;
+int knitPattern[255] = {0};
+
 void loop() {
+    parserSerialStream();
 }
+
+/// serial ///
+
+#define COM_CMD_PATTERN      'P'
+#define COM_CMD_FIRST_NEEDLE 'F'  //first needle of pattern from right
+#define COM_CMD_SEPERATOR    ':'
+#define COM_CMD_RESPONSE     'R'
+#define COM_CMD_PLOAD_END    '\n'
+
+// Parser states
+#define COM_PARSE_CMD      0x01
+#define COM_PARSE_SEP      0x02
+#define COM_PARSE_PLOAD    0x03
+
+unsigned char parserState = COM_PARSE_CMD;
+unsigned char parserReceivedCommand = 0;
+String parserReceivedPayload = "";
+
+void executeCommand(unsigned char cmd, String payload) {
+    switch (cmd) {
+    case COM_CMD_PATTERN:
+        sendCommand(COM_CMD_RESPONSE, "PatternLength: " + String(patternLength));
+        break;
+
+    case COM_CMD_FIRST_NEEDLE:
+        firstNeedle = payload.toInt();
+        sendCommand(COM_CMD_RESPONSE, "FirstNeedle: " + String(firstNeedle));
+        break;
+    }
+}
+
+void sendCommand(unsigned char cmd, String payload) {
+    Serial.write(cmd);
+    Serial.write(COM_CMD_SEPERATOR);
+    Serial.print(payload);
+    Serial.write("\n");
+}
+
+void parserSerialStream() {
+
+    if (Serial.available() == 0) {
+        return;
+    }
+
+    char buffer = Serial.read();
+
+    switch (parserState) {
+
+    case COM_PARSE_CMD:
+        parserState = COM_PARSE_SEP;
+        parserReceivedCommand = buffer;
+        parserReceivedPayload = "";
+        if (buffer == COM_CMD_PATTERN) {
+            patternLength = 0;
+        }
+
+        break;
+
+    case COM_PARSE_SEP:
+
+        // We're awaiting a seperator here, if not, back to cmd
+        if (buffer == COM_CMD_SEPERATOR) {
+            parserState = COM_PARSE_PLOAD;
+            break;
+        }
+
+        parserState = COM_PARSE_CMD;
+        break;
+
+    case COM_PARSE_PLOAD:
+
+        if (buffer == COM_CMD_PLOAD_END) {
+
+            executeCommand(parserReceivedCommand, parserReceivedPayload);
+            parserState = COM_PARSE_CMD;
+
+            sendCommand(COM_CMD_RESPONSE, "Received");
+            break;
+        }
+
+        if (parserReceivedCommand == COM_CMD_PATTERN) {
+            //Change state because the E6000 set the needle at '0'
+            knitPattern[patternLength] = (buffer == '0') ? 1 : 0;
+
+            patternLength += 1;
+        }
+        else {
+            parserReceivedPayload += buffer;
+        }
+
+        break;
+    }
+}
+
+
+///
 
 char pinA = LOW, pinB = LOW;
 unsigned long now, lastInterrupt, currentCursorPosition = 0;
@@ -26,7 +128,6 @@ unsigned long now, lastInterrupt, currentCursorPosition = 0;
 #define MIN_INTERRUPT 1000
 #define LTR 1
 #define RTL 2
-
 
 char direction;
 
@@ -54,8 +155,6 @@ void interruptA() {
         currentCursorPosition--;
         direction = LTR;
     }
-    Serial.print("Position: ");
-    Serial.println(currentCursorPosition);
 
 //    autoCalibrate(pinA, pinB);
 
@@ -66,14 +165,22 @@ void interruptA() {
 
     if (pinB == HIGH) {
         if (direction == RTL) {
-            int needle = currentCursorPosition / 2;
-            if (needle >= 40 && needle <= 80) {
-                digitalWrite(PIN_NEEDLE_LTR, (needle / 5) % 2);
+            int needle = - 27 + (currentCursorPosition / 2);
+            if (needle >= firstNeedle && needle < (firstNeedle + patternLength)) {
+                unsigned int index = patternLength - (needle - firstNeedle) - 1;
+                digitalWrite(PIN_NEEDLE_LTR, knitPattern[index]);
+            }
+            if (needle == firstNeedle + patternLength) {
+                Serial.println("E:1");
             }
         } else {
-            int needle = (currentCursorPosition + 25) / 2;
-            if (needle >= 40 && needle <= 80) {
-                digitalWrite(PIN_NEEDLE_RTL, (needle / 5) % 2);
+            int needle = - 27 + ((currentCursorPosition + 23) / 2);
+            if (needle >= firstNeedle && needle < (firstNeedle + patternLength)) {
+                unsigned int index = patternLength - (needle - firstNeedle) - 1;
+                digitalWrite(PIN_NEEDLE_RTL, knitPattern[index]);
+            }
+            if (needle == firstNeedle) {
+                Serial.println("E:1");
             }
         }
     }
